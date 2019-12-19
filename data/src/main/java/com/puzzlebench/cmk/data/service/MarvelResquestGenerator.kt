e10e3266509e4a917e2615fb5207b9e22c805a8b
German Andrade <german.andrade@globant.com>
2019-12-19 10:25:38 -0500

    Fixed requests to follow new params hash param is now a md5(ts+privateKey+publicKey) based on ts+privateKey+publicKey

--- data/src/main/java/com/puzzlebench/cmk/data/service/MarvelResquestGenerator.kt
+++ data/src/main/java/com/puzzlebench/cmk/data/service/MarvelResquestGenerator.kt
@@ -6,20 +6,23 @@ import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import java.math.BigInteger
+import java.security.MessageDigest
 
 
 class MarvelResquestGenerator {
-    private val PRIVATE_API_KEY_ARG = "hash"
+    private val HASH_ARG = "hash"
     private val PUBLIC_API_KEY_ARG = "apikey"
     private val TS = "ts"
     private val TS_VALUE = "1"
+    private val HASH_VALUE: String = (TS_VALUE + BuildConfig.PRIVATE_API_KEY_VALUE + BuildConfig.PUBLIC_API_KEY_VALUE).md5()
     private val httpClient = OkHttpClient.Builder().addInterceptor { chain ->
         val defaultRequest = chain.request()
 
         val defaultHttpUrl = defaultRequest.url()
         val httpUrl = defaultHttpUrl.newBuilder()
                 .addQueryParameter(PUBLIC_API_KEY_ARG, BuildConfig.PUBLIC_API_KEY_VALUE)
-                .addQueryParameter(PRIVATE_API_KEY_ARG, BuildConfig.PRIVATE_API_KEY_VALUE)
+                .addQueryParameter(HASH_ARG, HASH_VALUE)
                 .addQueryParameter(TS, TS_VALUE)
                 .build()
 
@@ -48,4 +51,9 @@ class MarvelResquestGenerator {
         val okHttpClient = httpClient.build()
         return makeMarvelService(okHttpClient)
     }
+}
+
+private fun String.md5(): String {
+    val md = MessageDigest.getInstance("MD5")
+    return BigInteger(1, md.digest(toByteArray())).toString(16).padStart(32, '0')
 }
