5ed78f03c83b5c18e6eaedc779426d33266adb0c
omar.bacilio <iomarbn@gmail.com>
2020-01-27 15:45:17 -0600

    Replace Picasso with Glide

--- data/src/main/java/com/puzzlebench/cmk/data/service/response/ThumbnailResponse.kt
+++ data/src/main/java/com/puzzlebench/cmk/data/service/response/ThumbnailResponse.kt
@@ -4,4 +4,9 @@ package com.puzzlebench.cmk.data.service.response
 class ThumbnailResponse(
         var path: String,
         var extension: String
-)
+) {
+    val securePath: String
+        get() {
+            return path.replace("http", "https")
+        }
+}
