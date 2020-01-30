34b74d2585b8f4671d16a4dabc782d126ea460f5
omar.bacilio <iomarbn@gmail.com>
2020-01-30 11:29:51 -0600

    Add cursor loader and thumbnail object in content provider query result

--- app/src/main/java/com/puzzlebench/clean_marvel_kotlin/presentation/mvp/CharacterView.kt
+++ app/src/main/java/com/puzzlebench/clean_marvel_kotlin/presentation/mvp/CharacterView.kt
@@ -1,22 +1,40 @@
 package com.puzzlebench.clean_marvel_kotlin.presentation.mvp
 
+import android.content.ContentUris
+import android.database.Cursor
+import android.os.Bundle
+import android.util.Log
 import android.view.View
+import androidx.loader.app.LoaderManager
+import androidx.loader.content.CursorLoader
+import androidx.loader.content.Loader
 import androidx.recyclerview.widget.GridLayoutManager
 import com.puzzlebench.clean_marvel_kotlin.R
 import com.puzzlebench.clean_marvel_kotlin.presentation.MainActivity
 import com.puzzlebench.clean_marvel_kotlin.presentation.adapter.CharacterAdapter
 import com.puzzlebench.clean_marvel_kotlin.presentation.dialog.CharacterDetailDialog
 import com.puzzlebench.clean_marvel_kotlin.presentation.extension.showToast
+import com.puzzlebench.clean_marvel_kotlin.presentation.provider.CharactersContentProvider
+import com.puzzlebench.clean_marvel_kotlin.presentation.provider.CharactersContentProvider.Companion.COLUMN_DESCRIPTION
+import com.puzzlebench.clean_marvel_kotlin.presentation.provider.CharactersContentProvider.Companion.COLUMN_ID
+import com.puzzlebench.clean_marvel_kotlin.presentation.provider.CharactersContentProvider.Companion.COLUMN_NAME
+import com.puzzlebench.clean_marvel_kotlin.presentation.provider.CharactersContentProvider.Companion.COLUMN_THUMBNAIL_EXTENSION
+import com.puzzlebench.clean_marvel_kotlin.presentation.provider.CharactersContentProvider.Companion.COLUMN_THUMBNAIL_PATH
 import com.puzzlebench.cmk.domain.model.Character
-import kotlinx.android.synthetic.main.activity_main.recycleView
-import kotlinx.android.synthetic.main.activity_main.progressBar
+import com.puzzlebench.cmk.domain.model.Thumbnail
+import kotlinx.android.synthetic.main.activity_main.*
 import java.lang.ref.WeakReference
 
-class CharacterView(activity: MainActivity) {
+class CharacterView(activity: MainActivity) : LoaderManager.LoaderCallbacks<Cursor> {
+
     private val activityRef = WeakReference(activity)
 
     private var adapter = CharacterAdapter { character ->
-        displayCharacterDetail(character)
+        // displayCharacterDetail(character)
+        val params = Bundle().apply {
+            putInt(SELECTED_CHARACTER_ARG, character.id)
+        }
+        LoaderManager.getInstance(activity).restartLoader(LOADER_SINGLE_ID, params, this)
     }
 
     private fun displayCharacterDetail(character: Character) {
@@ -31,6 +49,7 @@ class CharacterView(activity: MainActivity) {
             it.recycleView.layoutManager = GridLayoutManager(it, SPAN_COUNT)
             it.recycleView.adapter = adapter
             hideLoading()
+            // initializeLoader(it)
         }
     }
 
@@ -67,7 +86,73 @@ class CharacterView(activity: MainActivity) {
         }
     }
 
+    fun initializeLoader() {
+        runSafely {
+            LoaderManager.getInstance(it).initLoader(LOADER_ALL_ID, null, this)
+        }
+    }
+
+    override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> {
+        val characterId = args?.getInt(SELECTED_CHARACTER_ARG) ?: 0
+        val uri = if (characterId == 0) {
+            CharactersContentProvider.CONTENT_URI
+        } else {
+            ContentUris.withAppendedId(CharactersContentProvider.CONTENT_URI, characterId.toLong())
+        }
+
+        return activityRef.get()?.let {
+            CursorLoader(it, uri, null, null, null, null)
+        } ?: run {
+            throw Exception("Invalid activity")
+        }
+    }
+
+    override fun onLoadFinished(loader: Loader<Cursor>, data: Cursor?) {
+        if (data == null || data.count == 0) return
+
+        val characters = extractData(data)
+
+        if (loader.id == LOADER_ALL_ID) {
+            showCharacters(characters)
+        } else {
+            characters.firstOrNull()?.let { safeCharacter ->
+                displayCharacterDetail(safeCharacter)
+            }
+        }
+    }
+
+    override fun onLoaderReset(loader: Loader<Cursor>) = Unit
+
+    private fun extractData(cursor: Cursor): List<Character> {
+        val characters = mutableListOf<Character>()
+
+        if (cursor.moveToFirst()) {
+            do {
+                with(cursor) {
+                    val id = getInt(getColumnIndex(COLUMN_ID))
+                    val name = getString(getColumnIndex(COLUMN_NAME))
+                    val description = getString(getColumnIndex(COLUMN_DESCRIPTION))
+                    val thumbnailPath = getString(getColumnIndex(COLUMN_THUMBNAIL_PATH))
+                    val thumbnailExtension = getString(getColumnIndex(COLUMN_THUMBNAIL_EXTENSION))
+
+                    Log.i("Cursro", "Character = $id, $name, $description, $thumbnailPath, $thumbnailExtension")
+                    characters.add(Character(
+                            id,
+                            name,
+                            description,
+                            Thumbnail(thumbnailPath, thumbnailExtension))
+                    )
+                }
+            } while (cursor.moveToNext())
+        }
+
+        return characters
+    }
+
     companion object {
         private const val SPAN_COUNT = 1
+        private const val SELECTED_CHARACTER_ARG = "SELECTED_CHARACTER_ARG"
+        private const val LOADER_ALL_ID = 0
+        private const val LOADER_SINGLE_ID = 1
     }
 }
--- app/src/main/java/com/puzzlebench/clean_marvel_kotlin/presentation/provider/CharactersContentProvider.kt
+++ app/src/main/java/com/puzzlebench/clean_marvel_kotlin/presentation/provider/CharactersContentProvider.kt
@@ -12,8 +12,8 @@ import io.realm.Realm
 class CharactersContentProvider : ContentProvider() {
 
     private val uriMatcher = UriMatcher(UriMatcher.NO_MATCH).apply {
-        addURI(AUTHORITY, "CharacterRealm", ALL_CHARACTERS)
-        addURI(AUTHORITY, "CharacterRealm/#", SINGLE_CHARACTER)
+        addURI(AUTHORITY, "Characters", ALL_CHARACTERS)
+        addURI(AUTHORITY, "Characters/#", SINGLE_CHARACTER)
     }
 
     override fun insert(uri: Uri, values: ContentValues?): Uri? {
@@ -21,7 +21,13 @@ class CharactersContentProvider : ContentProvider() {
     }
 
     private val columns by lazy {
-        arrayOf(COLUMN_ID, COLUMN_NAME, COLUMN_DESCRIPTION)
+        arrayOf(
+                COLUMN_ID,
+                COLUMN_NAME,
+                COLUMN_DESCRIPTION,
+                COLUMN_THUMBNAIL_PATH,
+                COLUMN_THUMBNAIL_EXTENSION
+        )
     }
 
     override fun query(
@@ -49,12 +55,15 @@ class CharactersContentProvider : ContentProvider() {
                     matrixCursor.addRow(characterToArray(it))
                 }
             }
+            matrixCursor.setNotificationUri(context?.contentResolver, uri)
         }
 
         return matrixCursor
     }
 
-    private fun characterToArray(it: CharacterRealm) = arrayOf(it._id, it.name, it.description)
+    private fun characterToArray(it: CharacterRealm) = arrayOf(
+            it._id, it.name, it.description, it.thumbnail?.path, it.thumbnail?.extension
+    )
 
     override fun onCreate(): Boolean {
         context?.let {
@@ -86,12 +95,16 @@ class CharactersContentProvider : ContentProvider() {
     }
 
     companion object {
+        private const val URI = "content://com.puzzlebench.clean_marvel_kotlin/Characters"
+        val CONTENT_URI = Uri.parse(URI)
         private const val AUTHORITY = "com.puzzlebench.clean_marvel_kotlin"
         private const val ALL_CHARACTERS = 1
         private const val SINGLE_CHARACTER = 2
 
-        private const val COLUMN_ID = "_id"
-        private const val COLUMN_NAME = "name"
-        private const val COLUMN_DESCRIPTION = "description"
+        const val COLUMN_ID = "_id"
+        const val COLUMN_NAME = "name"
+        const val COLUMN_DESCRIPTION = "description"
+        const val COLUMN_THUMBNAIL_PATH = "path"
+        const val COLUMN_THUMBNAIL_EXTENSION = "extension"
     }
 }
